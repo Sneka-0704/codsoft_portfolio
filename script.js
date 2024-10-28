050e0721132d0dfbb00bf8e97e2b531469731fb1
Sneka-0704 <snekavarshini07@gmail.com>
2024-10-28 13:56:57 +0530

    first commit

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ $(window).on('scroll load',function(){
 });
 
 });
+
+function openModal(modalId) {
+  document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+  document.getElementById(modalId).style.display = "none";
+}
+
+// Close the modal when clicking outside of it
+window.onclick = function(event) {
+  const modals = document.querySelectorAll('.modal');
+  modals.forEach(modal => {
+      if (event.target === modal) {
+          modal.style.display = "none";
+      }
+  });
+}
